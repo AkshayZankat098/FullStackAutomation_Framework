b93059c5c4e87974271012f56cce274323e4ea8c
AkshayZankat098 <akshayzankat999@gmail.com>
2025-06-24 16:34:13 +0530

    24th june 2025 comapany

--- app/src/test/java/Runner/TestRunner.java
+++ app/src/test/java/Runner/TestRunner.java
@@ -1,6 +1,16 @@
 package Runner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(
+        features = "src/test/resources/Features",                   // ✅ Path to feature a file
+        glue = {"StepDefinitionFile"},                              // ✅ Step definition package
+        plugin = {"pretty", "html:target/cucumber-report.html"},    // ✅ Optional report
+        monochrome = true                                           // ✅ Cleaner console output
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
+
